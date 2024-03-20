48451d4144ae6d6d8efc643f037753794e58aca6
Gaurav Juneja <gjuneja555@gmail.com>
2024-03-20 21:17:58 +0530

    floyd

--- PRACTICE QUESTION ON BASICS OF C++/16-March/floyd.cpp
+++ PRACTICE QUESTION ON BASICS OF C++/16-March/floyd.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
+    int row_col,count=1;
+    cout<<"Enter the number of rows and columns: ";
+    cin>>row_col;
     
+    for(int i=1; i<=row_col; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+
 }
